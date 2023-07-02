e3d2ba9e1a0e0ac6fca3ca649c83c3d9143cc13e
Sahal P <112080987+Sahal-P@users.noreply.github.com>
2023-07-02 05:26:14 +0000

    added a way to solve using heap

--- top_k_frequent_element-347/code.py
+++ top_k_frequent_element-347/code.py
@@ -1,14 +1,25 @@
 import collections
 
-
+import heapq
 
 def topKFrequent(arr,k):
    dic=collections.defaultdict(int)
    for i in arr:
     dic[i]+=1
    result = sorted(dic, key=lambda x: dic[x], reverse=True)
+   print(result[:k])
    return result[:k]
 
 
 arr = [1,1,1,2,2,3]
 topKFrequent(arr,2)
+
+def usingHeap(arr, k):
+    countes = collections.Counter(nums)
+    min_heap = []
+    for el, freq in countes.items():
+      heapq.heappush(min_heap,(freq,el))
+      if len(min_heap)>k:
+         heapq.heappop(min_heap)
+    res=[num for el, num in min_heap]
+    return res
