de01a39c7497fe95cb44add00a2322c729a67de1
Sahal P <112080987+Sahal-P@users.noreply.github.com>
2023-07-04 16:28:11 +0530

    added brute force method

--- encode_and_decode_strings-271/code.py
+++ encode_and_decode_strings-271/code.py
@@ -1,2 +1,21 @@
-def solutionBrute(arr):
-    pass
+
+def encodedecode(arr):
+  encoded =""
+  for i in arr:
+    lent = len(i)
+    encoded+=(f"{lent}#{i}")
+  decoded =[]
+  num=""
+  for t,j in enumerate(encoded):
+    if j.isdigit():
+      num+=j
+    if j == "#":
+      word=""
+      for k in range(1,int(num)+1):
+        word+=encoded[t+k]
+      decoded.append(word)
+      word=""
+      num=""
+      
+  print(decoded)
+    
