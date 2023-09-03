59e82a027dcb903ab0e9606e1a41090110d66bf8
Sahal-P <sahlcyprus@gmail.com>
2023-09-03 11:41:52 +0530

    added linear time solution

--- best_time_to_buy_and_sell_stock-121/code.py
+++ best_time_to_buy_and_sell_stock-121/code.py
@@ -2,13 +2,31 @@ from typing import List
 from testcase import testcase
 
 # Brute Force Method Runs in O(n2)
-def maxProfit( prices: List[int] ) -> int:
+def maxProfit_b( prices: List[int] ) -> int:
     dif = 0
     for i in range(len(prices)):
         for j in range(i+1,len(prices)):
             val = prices[j]-prices[i]
             dif = max(dif,val)
     return dif
+
+
+
+# Linear Time Solution O(n)
+
+def maxProfit( prices: List[int] ) -> int:
+    left , right = 0, 1
+    maxProfit = 0
+    
+    while right < len(prices):
+        if prices[left] < prices[right]:
+            profit  = prices[right] - prices[left]
+            maxProfit = max(maxProfit,profit)
+        else:
+            left = right
+        right +=1
+    return maxProfit
+
 
 case1 = [2,4,1]
 case2 = [7,1,5,3,6,4]
