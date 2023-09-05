0e77678f55721b531c611e771d4c24dc47732715
Sahal-P <sahlcyprus@gmail.com>
2023-09-05 08:51:54 +0530

    solved using sliding window

--- longest_repeating_character_replacement-424/code.py
+++ longest_repeating_character_replacement-424/code.py
@@ -2,7 +2,7 @@ from collections import Counter
 from testcase import testcase
 
 # the time complexity of the provided algorithm is O(n^3 * k) in the worst case, and the space complexity is O(k).
-def characterReplacement(s: str, k: int) -> int:    
+def characterReplacement_b(s: str, k: int) -> int:    
     longest = 0
     for i in range(len(s)):
         for j in range(i+1, len(s)+1):
@@ -12,10 +12,20 @@ def characterReplacement(s: str, k: int) -> int:
                 if (j-i) - count[v] <= k:
                     longest = max(longest,(j-i))
     return longest
-    
-            
-        
-s = "AAAB"
+
+# Linear time solution O(26*n) which O(n)
+def characterReplacement(s: str, k: int) -> int:    
+    count = {}
+    res = 0
+    left = 0
+    for right in range(len(s)):
+        count[s[right]] = 1+ count.get(s[right], 0)
+        if (right-left+1) - max(count.values()) >k:
+            count[s[left]]-=1
+            left+=1
+        res = max(res, (right-left+1))
+    return res
+s = "AABABBA"
 k = 1
 
 testcasek= 100
