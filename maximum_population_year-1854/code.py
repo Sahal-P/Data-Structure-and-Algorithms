f7b3b233b36ce12610c706ada1ebe34a5810ceb7
Sahal-P <sahlcyprus@gmail.com>
2023-09-05 23:00:20 +0530

    initialized

---
+++ maximum_population_year-1854/code.py
@@ -0,0 +1,21 @@
+from typing import List
+
+def maximumPopulation( logs: List[List[int]] ) -> int:
+    print(logs)
+    return 1993
+    
+    
+    
+logs_1 = [[1993,1999],[2000,2010]]
+output_1 = 1993
+# Explanation: The maximum population is 1, and 1993 is the earliest year with this population.
+
+logs_2 = [[1950,1961],[1960,1971],[1970,1981]]
+output_2 = 1960
+
+# Explanation: 
+# The maximum population is 2, and it had happened in years 1960 and 1970.
+# The earlier year between them is 1960.
+
+assert maximumPopulation(logs_1) == output_1
+assert maximumPopulation(logs_2) == output_2
