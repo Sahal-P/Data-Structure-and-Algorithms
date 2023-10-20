162f9e1bee246d72f83705381228b9b37d1d85a2
Sahal-P <sahlcyprus@gmail.com>
2023-10-20 09:42:03 +0530

    solved: T O(n) M O(n)

---
+++ linked_list_cycle-141/code.py
@@ -0,0 +1,49 @@
+from typing import Optional
+
+class Node:
+    def __init__(self, data) -> None:
+        self.data = data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self) -> None:
+        self.head = None
+        
+    def insert_at_end(self, data):
+        for val in range(len(data)):
+            new_node = Node(data[val])
+            if self.head is None:
+                self.head = new_node
+                continue
+
+            current_node = self.head
+            while (current_node.next):
+                current_node = current_node.next
+
+            current_node.next = new_node
+
+
+linkl = SinglyLinkedList()
+linkl.insert_at_end([3,2,0,-4])
+
+cHead = linkl.head
+tHead = linkl.head
+
+while cHead:
+    prev = cHead
+    cHead = cHead.next
+    
+prev.next = tHead.next
+
+
+
+def hasCycle(head: Optional[Node]) -> bool:
+    hashSet = set()
+    while head:
+        hashSet.add(head)
+        if head.next in hashSet:
+            return True
+        head = head.next
+    return False
+    
+hasCycle(tHead)
