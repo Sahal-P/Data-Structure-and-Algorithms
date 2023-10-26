fec2c024e65a74290d773c72383b2679eb67b37c
Sahal-P <sahlcyprus@gmail.com>
2023-10-26 23:08:31 +0530

    solved: T O(1) for getting the minimum

---
+++ min_stack-155/code.py
@@ -0,0 +1,31 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        val = min(val, self.minStack[-1] if self.minStack else val)
+        self.minStack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minStack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
+
+
+val = [0,-2,4]
+
+obj = MinStack()
+for i in range(len(val)):
+    obj.push(val[i])
+obj.pop()
+param_3 = obj.top()
+param_4 = obj.getMin()
+print(param_3,param_4)
