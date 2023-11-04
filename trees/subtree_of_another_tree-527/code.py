b95fc6e7252b0af11c6c2b15df884526b3d70101
Sahal-P <sahlcyprus@gmail.com>
2023-11-04 09:38:29 +0530

    solved: T O(h) M O(h)

---
+++ trees/subtree_of_another_tree-527/code.py
@@ -0,0 +1,34 @@
+from typing import Optional
+
+class Node:
+   def __init__(self, data):
+      self.data = data
+      self.left = None
+      self.right = None
+      
+root = Node(3)
+root.right = Node(5)
+root.left = Node(4)
+root.left.left = Node(1)
+root.left.right = Node(2)
+
+subroot = Node(4)
+subroot.right = Node(2)
+subroot.left = Node(1)
+
+def isSubtree(root: Optional[Node], subRoot: Optional[Node]) -> bool:
+    if not subRoot: return True
+    if not root: return False
+    
+    if isSametree(root, subRoot):
+        return True
+    return (isSubtree(root.left, subRoot) or isSubtree(root.right, subRoot))
+    
+def isSametree(root: Optional[Node], subRoot: Optional[Node]) -> bool:
+    if not root and not subRoot:
+        return True
+    if root and subRoot and root.data == subRoot.data:
+        return (isSametree(root.left, subRoot.left) and isSametree(root.right, subRoot.right))
+    return False
+    
+print(isSubtree(root, subroot))
