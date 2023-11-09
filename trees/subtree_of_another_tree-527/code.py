13bb8ec5bce97f078a41f9f92b5f3796acf7166c
Sahal-P <sahlcyprus@gmail.com>
2023-11-09 23:43:26 +0530

    added: Node type

--- trees/subtree_of_another_tree-527/code.py
+++ trees/subtree_of_another_tree-527/code.py
@@ -3,8 +3,8 @@ from typing import Optional
 class Node:
    def __init__(self, data):
       self.data = data
-      self.left = None
-      self.right = None
+      self.left: Node | None = None
+      self.right: Node | None  = None
       
 root = Node(3)
 root.right = Node(5)
@@ -16,11 +16,14 @@ subroot = Node(4)
 subroot.right = Node(2)
 subroot.left = Node(1)
 
-# 
+# T O(h) M O(h)
+
+#  T O(M-N when i get the job i will start to get)
+
 def isSubtree(root: Optional[Node], subRoot: Optional[Node]) -> bool:
     if not subRoot: return True
     if not root: return False
-    
+
     if isSametree(root, subRoot):
         return True
     return (isSubtree(root.left, subRoot) or isSubtree(root.right, subRoot))
