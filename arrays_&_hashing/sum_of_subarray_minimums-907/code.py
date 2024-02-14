3c2cbb2c79f437fc1e9f90bcc29933e8c0ecd176
Sahal-P <sahlcyprus@gmail.com>
2024-02-14 23:04:54 +0530

    initialized sumSubarrayMins

---
+++ arrays_&_hashing/sum_of_subarray_minimums-907/code.py
@@ -0,0 +1,11 @@
+from typing import List
+
+def sumSubarrayMins(arr: List[int]) -> int:
+    MOD = 10**9 + 7
+    arr_len = len(arr)
+    
+    print(arr, MOD)
+    
+
+arr = [3,1,2,4]
+sumSubarrayMins(arr)
