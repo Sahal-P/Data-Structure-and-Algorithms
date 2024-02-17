6421e9e60f1fd57cd940139240ed43db9eaa77b3
Sahal-P <sahlcyprus@gmail.com>
2024-02-17 23:26:38 +0530

    solved using max heap

--- heap_priority_queue/furthest_building_you_can_reach-1642/code.py
+++ heap_priority_queue/furthest_building_you_can_reach-1642/code.py
@@ -1,12 +1,32 @@
 from typing import List
+import heapq
+
+
+# O(n logn)
+def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
+    heap = []  # max heap of bricks
+    length = len(heights) - 1
+
+    for i in range(length):
+        diff = heights[i + 1] - heights[i]
+        if diff <= 0:
+            continue
+        bricks -= diff
+        heapq.heappush(heap, -diff)
+        if bricks < 0:
+            if ladders == 0:
+                return i
+            ladders -= 1
+            add_bricks = -heapq.heappop(heap)
+            print(add_bricks, i, diff)
+            bricks += add_bricks
+
+    return length
+
 
 
 def furthestBuilding(heights: List[int], bricks: int, ladders: int) -> int:
-    print(len(heights))
-    ladders_, bricks_ = ladders, bricks
     i = 0
-    j = 0
-
     while i < len(heights) - 1:
         if heights[i] >= heights[i + 1]:
             i += 1
@@ -25,21 +45,4 @@ def furthestBuilding(heights: List[int], bricks: int, ladders: int) -> int:
                 i += 1
                 continue
 
-    while j < len(heights) - 1:
-        if heights[j] >= heights[j + 1]:
-            j += 1
-            continue
-        else:
-            diff = heights[j + 1] - heights[j]
-            if ladders_ > 0:
-                ladders_ -= 1
-                j += 1
-                continue
-            elif bricks_ >= diff:
-                bricks_ -= diff
-                j += 1
-                continue
-            else:
-                break
-
-    return max(i, j)
+    return i
